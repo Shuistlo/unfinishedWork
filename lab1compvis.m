6c6b6851d39aa4edc853f9db06cd26191e6c6b8d
Shuistlo <aredpenguin1@yahoo.com>
2018-11-13 11:52:28 +0000

    Update lab1compvis.m

--- lab1compvis.m
+++ lab1compvis.m
@@ -17,12 +17,58 @@ function c = corr130(img, s)
     c = corr2(I,Is);
 end
 
-
-x = 0:30;
-y = [];
+term = 0:29; %trying to make a vector of values 0 to 29
+roos = 0:29;
+wood = 0:29;
 counter = 0;
-while counter< 
-    
+while counter< 30
+    roos(counter) = corr130(Ir, counter);
+    wood(counter) = corr130(Iw, counter);
+    counter = counter +1;
 end
 
-scatter(x,y);
+scatter(term, roos); %plot these correctly in the same figure
+scatter(term, wood);
+
+%Plot a graph of shift vs correlation coefficient for both the rooster and the woods image.
+%Include in your report a print out of this figure and a print out of your code. Briefly describe and
+%explain the results you have obtained
+
+gaus6 = fspecial('gaussian', 5,6);
+gaus2 = fspecial('gaussian', 5,2);
+DoG = gaus6 - gaus2;
+
+Rconv = conv2(Ir, DoG, 'same');
+Wconv = conv2(Iw, DoG, 'same');
+
+counter = 0;
+while counter< 30
+    roos(counter) = corr130(Rconv, counter);
+    wood(counter) = corr130(Wconv, counter);
+    counter = counter +1;
+end
+
+scatter(term, roos); %plot these properly
+scatter(term, wood);
+
+gaus4 = fspecial('gaussian', 5, 4);
+gaus05 = fspecial('gaussian', 5, 0.5);
+DoG = gaus4 - gaus05;
+
+Rconv = conv2(Ir, DoG, 'same');
+Wconv = conv2(Iw, DoG, 'same');
+
+counter = 0;
+while counter< 30
+    roos(counter) = corr130(Rconv, counter);
+    wood(counter) = corr130(Wconv, counter);
+    counter = counter +1;
+end
+
+scatter(term, roos); %plot these properly
+scatter(term, wood);
+
+%Plot graphs of shift vs correlation coefficient for both the rooster and the woods image after
+%convolution with both the DoG masks. Include in your report a print out of this figure. Briefly
+%explain why the results for the two DoG masks differ, also explain why the current results differ
+%from those in the previous section.
